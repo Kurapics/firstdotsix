012978c45f948b4f6e3744b89169450f4ce51be8
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:40:28 +0200

    Добавьте файлы проекта.

---
+++ firstdotsix/Program.cs
@@ -0,0 +1,16 @@
+﻿using firstdotsix;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите n: ");
+        double n = double.Parse(Console.ReadLine());
+        Console.Write("Введите x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Solution solution = new Solution();
+
+        Console.WriteLine(solution.Method(n, x));
+    }
+}
---
+++ firstdotsix/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstdotsix
+{
+    internal class Solution
+    {
+        public double Method(double x, double n)
+        {
+            double res = 0;
+            for (int i = 1; i <= n; i++)
+                res += i / Math.Pow(x, i);
+            return res;
+        }
+    }
+}
